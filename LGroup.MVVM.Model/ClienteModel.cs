d84e82b9c4662c9206ca48c86d9d5712952409d0
erick.workspace <erick.workspace@hotmail.com>
2016-01-30 12:59:28 -0200

    Inicio da estrutura do prjeto

---
+++ LGroup.MVVM.Model/ClienteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGroup.MVVM.Model
+{
+ public sealed    class ClienteModel
+    {
+        public String Nome { get; set; }
+        public String  Email { get; set; }
+        public String Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
